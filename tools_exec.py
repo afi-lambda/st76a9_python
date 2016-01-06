2dc98d19256e49c350dc42df7f9cafff56424ee9
Alain Fischer <alain.fischer@bluewin.ch>
2016-01-06 18:53:07 +0100

    Added simulator_evaluateAll function

--- tools_exec.py
+++ tools_exec.py
@@ -11,13 +11,19 @@ Exec = autoclass('st76.tools.Exec')
 SourcecodeRef = autoclass('st76.io.SourcecodeRef')
 Simulator = autoclass('st76.simulator.Simulator')
 
+def simulator_evaluateAll(fileRef):
+    ci = fileRef.chunks()
+    while ci.hasMoreChunks():
+        chunk = ci.nextChunk()
+        Simulator.evaluate(chunk)
+
 def exec_main():
     Runtime.initialize()
     HostSystemBuilder.defineClasses()
     HostSystemBuilder.defineBootSupport()
     Runtime.Smalltalk.defineAs(UniqueString._for("HasGUI"), Obj.FALSE)
     ref = SourcecodeRef.create('jar:/source/bootstrap.utf.txt', Exec)
-    Simulator.evaluateAll(ref)
+    Simulator_evaluateAll(ref)
 
 if __name__ == '__main__':
     exec_main()
