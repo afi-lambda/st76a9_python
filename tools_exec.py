264d1552b3249350e18b0b571deca9a15c6784d1
Alain Fischer <alain.fischer@bluewin.ch>
2016-01-06 18:49:11 +0100

    Added exec_main function

---
+++ tools_exec.py
@@ -0,0 +1,23 @@
+import os
+os.environ['CLASSPATH'] = 'st76a9.jar'
+
+from jnius import autoclass
+
+Runtime = autoclass('st76.runtime.Runtime')
+HostSystemBuilder = autoclass('st76.simulator.host.HostSystemBuilder')
+UniqueString = autoclass('st76.runtime.UniqueString')
+Obj = autoclass('st76.runtime.Obj')
+Exec = autoclass('st76.tools.Exec')
+SourcecodeRef = autoclass('st76.io.SourcecodeRef')
+Simulator = autoclass('st76.simulator.Simulator')
+
+def exec_main():
+    Runtime.initialize()
+    HostSystemBuilder.defineClasses()
+    HostSystemBuilder.defineBootSupport()
+    Runtime.Smalltalk.defineAs(UniqueString._for("HasGUI"), Obj.FALSE)
+    ref = SourcecodeRef.create('jar:/source/bootstrap.utf.txt', Exec)
+    Simulator.evaluateAll(ref)
+
+if __name__ == '__main__':
+    exec_main()
