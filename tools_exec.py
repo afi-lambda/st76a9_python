20e97bb300c181f8a3c8044effbbf1858d7ed014
Alain Fischer <alain.fischer@bluewin.ch>
2016-01-07 18:49:36 +0100

    added simulator_evaluate function

--- tools_exec.py
+++ tools_exec.py
@@ -10,12 +10,25 @@ Obj = autoclass('st76.runtime.Obj')
 Exec = autoclass('st76.tools.Exec')
 SourcecodeRef = autoclass('st76.io.SourcecodeRef')
 Simulator = autoclass('st76.simulator.Simulator')
+Transcoder = autoclass('st76.io.Transcoder')
+Compiler = autoclass('st76.compiler.Compiler')
+ByteString = autoclass('st76.runtime.ByteString')
+Int = autoclass('st76.runtime.Int')
+Integer = autoclass('java.lang.Integer')
+Context = autoclass('st76.simulator.Context')
+
+def simulator_evaluate(expression):
+     altoSource = Transcoder.toAlto("doIt [^[" + expression + "]]")
+     objectCls = Runtime.Smalltalk._ref(UniqueString._for("Object"))
+     method_tuple = Compiler().compileIn(ByteString(altoSource), objectCls)
+     method = method_tuple._ref(Integer(2))
+     return Simulator(Context().set(None, Obj.NIL, objectCls, method)).run()
 
 def simulator_evaluateAll(fileRef):
     ci = fileRef.chunks()
     while ci.hasMoreChunks():
         chunk = ci.nextChunk()
-        Simulator.evaluate(chunk)
+        simulator_evaluate(chunk)
 
 def exec_main():
     Runtime.initialize()
