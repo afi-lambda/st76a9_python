7a437d981f85ae5ef102dd7651d000e7c44b13d3
Alain Fischer <alain.fischer@bluewin.ch>
2016-01-11 18:49:31 +0100

    Added python Simulator class

--- tools_exec.py
+++ tools_exec.py
@@ -2,6 +2,7 @@ import os
 os.environ['CLASSPATH'] = 'st76a9.jar'
 
 from jnius import autoclass
+from jnius import JavaException
 
 Runtime = autoclass('st76.runtime.Runtime')
 HostSystemBuilder = autoclass('st76.simulator.host.HostSystemBuilder')
@@ -9,13 +10,26 @@ UniqueString = autoclass('st76.runtime.UniqueString')
 Obj = autoclass('st76.runtime.Obj')
 Exec = autoclass('st76.tools.Exec')
 SourcecodeRef = autoclass('st76.io.SourcecodeRef')
-Simulator = autoclass('st76.simulator.Simulator')
 Transcoder = autoclass('st76.io.Transcoder')
 Compiler = autoclass('st76.compiler.Compiler')
 ByteString = autoclass('st76.runtime.ByteString')
 Int = autoclass('st76.runtime.Int')
 Integer = autoclass('java.lang.Integer')
 Context = autoclass('st76.simulator.Context')
+
+class Simulator:
+    def __init__(self, context):
+        self.active_context = context
+
+    def run(self):
+        active_context = self.active_context
+        try:
+            while True:
+                active_context = active_context.step()
+        except JavaException, e:
+            if e.classname == 'st76.simulator.ReturnValue':
+                return e.exception.value()
+            raise e
 
 def simulator_evaluate(expression):
      altoSource = Transcoder.toAlto("doIt [^[" + expression + "]]")
@@ -37,6 +51,7 @@ def exec_main():
     Runtime.Smalltalk.defineAs(UniqueString._for("HasGUI"), Obj.FALSE)
     ref = SourcecodeRef.create('jar:/source/bootstrap.utf.txt', Exec)
     simulator_evaluateAll(ref)
+    print simulator_evaluate('3 + 4')
 
 if __name__ == '__main__':
     exec_main()
