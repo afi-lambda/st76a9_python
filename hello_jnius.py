8ec6c12bc066002f2002c756008ef53dce20d041
Alain Fischer <alain.fischer@studer-innotec.com>
2020-06-07 11:10:44 +0200

    Modified to be compatible with Python version 2 and 3

--- hello_jnius.py
+++ hello_jnius.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from jnius import autoclass
 
 Stack = autoclass('java.util.Stack')
@@ -5,5 +6,5 @@ stack = Stack()
 stack.push('hello')
 stack.push('world')
 
-print stack.pop() # --> 'world'
-print stack.pop() # --> 'hello'
+print(stack.pop()) # --> 'world'
+print(stack.pop()) # --> 'hello'
