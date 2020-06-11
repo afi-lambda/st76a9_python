cb65393c0cc3864a92ffd40e6fe729a6766e0711
Alain Fischer <alain.fischer@studer-innotec.com>
2020-06-11 13:40:25 +0200

    Improved code format

--- benchFib.py
+++ benchFib.py
@@ -1,12 +1,20 @@
 from __future__ import print_function
+
+
 def benchFib(n):
     if n < 2:
         return 1
     else:
         return benchFib(n - 1) + benchFib(n - 2) + 1
 
-print(benchFib(1))
-print(benchFib(2))
-print(benchFib(3))
-print(benchFib(4))
-print(benchFib(5))
+
+def main():
+    print(benchFib(1))
+    print(benchFib(2))
+    print(benchFib(3))
+    print(benchFib(4))
+    print(benchFib(5))
+
+
+if (__name__ == '__main__'):
+    main()
--- hello_jnius.py
+++ hello_jnius.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
 from jnius import autoclass
 
-Stack = autoclass('java.util.Stack')
-stack = Stack()
-stack.push('hello')
-stack.push('world')
 
-print(stack.pop()) # --> 'world'
-print(stack.pop()) # --> 'hello'
+def main():
+    Stack = autoclass('java.util.Stack')
+    stack = Stack()
+    stack.push('hello')
+    stack.push('world')
+    print(stack.pop())  # --> 'world'
+    print(stack.pop())  # --> 'hello'
+
+
+if (__name__ == '__main__'):
+    main()
